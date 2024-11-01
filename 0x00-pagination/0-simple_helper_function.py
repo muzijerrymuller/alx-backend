1f9178c9006a18b6a917e4f83ca590906532ab9c
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-01 09:16:33 +0000

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,7 +9,8 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Pagination helper function that retrieves range from given page & page size
+    Pagination helper function that retrieves
+    range from given page & page size
     """
     starting = (page - 1) * page_size
     ending = start + page_size
