50c931caf7d08dcfb4881ab0650cd56b224151a9
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-01 14:56:40 +0000

    paginate

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,6 +40,49 @@ class Server:
 
         return self.__dataset
 
+            return []
+        return data[start:end]#!/usr/bin/env python3
+"""
+Fundamental Pagination Sample.
+"""
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Derives the range of indices based on the given page
+    number and size specifications.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """
+    Server Class for Managing and Paginating Baby Names Database Records.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """
+        Configures a new server instance.
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        saved data set for an improvement of performance.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         saves data from corresponding data set.
