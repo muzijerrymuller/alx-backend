242aa4fe161cdc5cfad70dbb3d49d1831a18c4ae
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-05 09:21:24 +0000

    app, index

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Flask Application with Babel
+Flask Application with Babel and Locale Selection
 This module initializes a Flask web application with internationalization
-support using Babel. Available languages are English and French.
+support using Babel, and selects the best language match for each request.
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 class Config:
@@ -21,6 +21,16 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """
+    Selects the best match for supported languages based on the request's
+    'Accept-Language' headers.
+    Returns:
+        str: The best matching language code ('en' or 'fr').
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def home():
