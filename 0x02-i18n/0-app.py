cc162653416d1c741e35daceae907e353db618ad
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-06 10:29:56 +0000

    app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,6 +11,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """
@@ -18,6 +19,7 @@ def home():
     Returns:Rendered HTML template for the index page.
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
