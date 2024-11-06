bc4a542a2ac499426509f7c88d802ce20a739b0b
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-06 10:50:24 +0000

    1 app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -9,18 +9,23 @@ support using Babel, and selects the best language match for each request.
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
     """
-    Configuration class for setting up available languages, default locale, and timezone.
+    Configuration class for setting up available languages,
+    default locale, and timezone.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -32,6 +37,7 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def home():
     """
@@ -40,6 +46,7 @@ def home():
         Rendered HTML template for the index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
